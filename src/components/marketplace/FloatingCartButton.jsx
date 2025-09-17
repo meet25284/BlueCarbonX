c668b54cdd54443f680eb6c57ad2bfc6dc1bbcdc
Pansuriya Meet <meetpansuriya93@gmail.com>
2025-09-18 01:30:20 +0530

    add floating cart

--- src/components/marketplace/FloatingCartButton.jsx
+++ src/components/marketplace/FloatingCartButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import img from '/cart.png'
 export default function FloatingCartButton({ cartItemCount }) {
   return (
     <div className="mc-fab">
       <Link to="/marketplace/cart" className="text-white">
-        <span className="text-xl">🛒</span>
+        <span className="text-xl"><img src={img} height='27px' width='28px'/></span>
       </Link>
     </div>
   )
